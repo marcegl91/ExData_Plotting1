14572967adfda0ebdbf1c05b3b0561e7c535ebdb
Marcelo Guzman <marcegl91@gmail.com>
2015-01-06 23:47:15 +0000

    Third plot ready

--- plot2.R
+++ plot2.R
@@ -7,7 +7,5 @@ source("common.R")
 epc <- common()                                                                                                         
 png(filename = "plot2.png", width = 480, height = 480)
 
-## Because English is not my main language (only tested in Microsoft Windows)
-##Sys.setlocale("LC_TIME", "English")
 plot(epc$Time, epc$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)") 
 dev.off()
