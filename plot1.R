6164011b756b1c5b2b76faa4b91579e337939d65
Marcelo Guzman <marcegl91@gmail.com>
2015-01-06 20:21:14 +0000

    Few fixes

--- plot1.R
+++ plot1.R
@@ -1,10 +1,13 @@
 if(!file.exists("common.R")) {
     		writeLines("ERROR: common.R doesn't exist")
     		return
-}
+} else writeLines("Common.R file exists")
 
 source("common.R")
-common()
+epc <- common()
 
-hist(epc$Global_active_power, col = "red",
- xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+png(filename = "plot1.png", width = 480, height = 480)
+
+hist(epc$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+dev.off()
